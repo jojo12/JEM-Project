4225d93cf8a08df4dc9cc71e9b21e0dfe4eb9d3a
Jojo Murer <jojo.murer@bluewin.ch>
2025-12-15 10:57:18 +0100

    Update namespace for Joomla filesystem classes

--- admin/models/source.php
+++ admin/models/source.php
@@ -9,8 +9,8 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\Filesystem\Path;
-use Joomla\CMS\Filesystem\File;
+use Joomla\Filesystem\Path;
+use Joomla\Filesystem\File;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\MVC\Model\AdminModel;
 use Joomla\CMS\Component\ComponentHelper;
